9e732e113193767af375f8480e85b03b8c132e63
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2021-11-26 08:31:03 -0500

    clean & comment

--- 2015-data/fill_rtree_csv.hpp
+++ 2015-data/fill_rtree_csv.hpp
@@ -2,10 +2,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace csv;
 
+// (there are fancier ways to do this automatically with 
+// preprocessor directives but for now this will do)
+
 /* if __cpp_lib_experimental_filesystem */
-0
+
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 
@@ -15,27 +19,32 @@ namespace fs = std::experimental::filesystem;
 // namespace fs = filesystem;
 
 
+// Outputs a vector of file directories in a select path 
 static std::vector<fs::path> files_in_folder(fs::path path){
 	std::vector<fs::path> fileNamePaths;
 	if (!exists(path))
 		return fileNamePaths;
 	
 	for (const auto& entry : fs::directory_iterator(path))
-		// fileNamePaths.push_back(fs::path(entry).filename());
 		fileNamePaths.push_back(entry);
 	
 	return fileNamePaths;
 }
 
+/* CSV STRUCTURE */
+
 // fhv tripdata: IGNORE
 // green tripdata:
-	// node name: lpep_pickup_datetime
-	// y: Pickup_longitude
-	// x: Pickup_latitude
+	// - node name: lpep_pickup_datetime
+	// - y: Pickup_longitude
+	// - x: Pickup_latitude
 // yellow tripdata:
-	// node name: tpep_pickup_datetime
-	// y: pickup_longitude
-	// x: pickup_latitude
+	// - node name: tpep_pickup_datetime
+	// - y: pickup_longitude
+	// - x: pickup_latitude
+
+// After insertion is implemented: Receives a 2d rtree storing std::string records,
+// inserting the (lat, long) -> date&time data obtained from parsing the csv files.
 
 bool rtree_filler(){
 	std::vector<fs::path>  vec = files_in_folder("./data/");
@@ -56,9 +65,9 @@ bool rtree_filler(){
 			case 'g':
 				std::cout << "green csv\n";
 				for (auto& row: reader){
-					auto node_name = row["lpep_pickup_datetime"].get<std::string>();
-					auto y = row["Pickup_longitude"].get<double>();
-					auto x = row["Pickup_latitude"].get<double>();
+					std::string node_name = row["lpep_pickup_datetime"].get<std::string>();
+					double y = row["Pickup_longitude"].get<double>();
+					double x = row["Pickup_latitude"].get<double>();
 
 					std::cout << node_name << " - " << y << " - " << x <<"\n";
 				}
@@ -69,9 +78,9 @@ bool rtree_filler(){
 			case 'y':
 				std::cout << "yellow csv\n";
 				for (auto& row: reader){
-					auto node_name = row["tpep_pickup_datetime"].get<std::string>();
-					auto y = row["pickup_longitude"].get<double>();
-					auto x = row["pickup_latitude"].get<double>();
+					std::string node_name = row["tpep_pickup_datetime"].get<std::string>();
+					double y = row["pickup_longitude"].get<double>();
+					double x = row["pickup_latitude"].get<double>();
 
 					std::cout << node_name << " - " << y << " - " << x <<"\n";
 				}
