e630258fcc4c3a81aab699ae46efc51a06ef089d
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-11-26 04:58:16 -0500

    use an even number

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include <deque>
 
 
-using point_type = geom::Point<int, 4>;
+using point_type = geom::Point<int, 3>;
 using box_type = geom::Box<point_type>;
-using index_type = index::RTree<int, box_type, 3>;
+using index_type = index::RTree<int, box_type, 4>;
 
 struct TreePrinter {
-    using Tree = index::RTree<int, box_type, 3>;
+    using Tree = index_type;
 
     struct TextBlock {
         std::deque<std::string> lines;
@@ -159,13 +159,13 @@ int main () {
     box_type testbox2({ 2, 3, 4 }, { 2, 3, 4 });
     box_type testbox3({ 3, 4, 5 }, { 3, 4, 5 });
     box_type testbox4({ 4, 5, 6 }, { 4, 5, 6 });
-//    box_type testbox5({ 5, 6, 7 }, { 5, 6, 7 });
+    box_type testbox5({ 5, 6, 7 }, { 5, 6, 7 });
 
     tree.insert(testbox1, 3);
     tree.insert(testbox2, 4);
     tree.insert(testbox3, 5);
     tree.insert(testbox4, 6);
-//    tree.insert(testbox5, 7);
+    tree.insert(testbox5, 7);
 
     box_type query_box({ 1, 2, 3 }, { 4, 5, 6 });
     std::vector<int> result;
