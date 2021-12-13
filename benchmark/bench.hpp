5d89ab90656935fb69c9ad0956a9e22f54059b70
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2021-12-13 04:32:00 -0500

    boost rtree, simple comparison for testing purposes

---
+++ benchmark/bench.hpp
@@ -0,0 +1,93 @@
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
+// to store queries results
+#include <vector>
+#include <string>
+
+// just for output
+#include <iostream>
+#include "../2015-data/csv-parser/fast.h"
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+
+typedef bg::model::point<float, 2, bg::cs::cartesian> point;
+typedef bg::model::box<point> box;
+typedef std::pair<box, std::string> value;
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+// Outputs a vector of file directories in a select path 
+static std::vector<fs::path>
+files_in_folder_ (const fs::path& path) {
+    std::vector<fs::path> fileNamePaths;
+    if (!exists(path)) {
+        return fileNamePaths;
+    }
+
+    for (const auto& entry : fs::directory_iterator(path)) {
+        fileNamePaths.push_back(entry);
+    }
+
+    return fileNamePaths;
+}
+
+// create the rtree using default constructor
+bgi::rtree< value, bgi::quadratic<16> > rtree;
+
+void try_fetch_locations_ (const fs::path& path);
+
+bgi::rtree< value, bgi::quadratic<16> > &
+fetch_locations_ (const fs::path& folder) {
+    std::vector<fs::path> vec = files_in_folder_(folder);
+    
+    // benchmarking
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+    for (const fs::path& entry : vec) {
+        try_fetch_locations_(entry);
+    }
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    std::cout << "Records inserted in " << time_span.count() << " seconds." << std::endl;
+    return rtree;
+}
+
+void try_fetch_locations_ (const fs::path& path) {
+
+    io::CSVReader<3,io::trim_chars<>, io::no_quote_escape<','>> in(path.u8string());
+    std::string file = path.filename().string();
+
+    std::string pickup_label = file[0] == 'g' ? "lpep_pickup_datetime" : "tpep_pickup_datetime";
+    std::string plon_label = file[0] == 'g' ? "Pickup_longitude" : "pickup_longitude";
+    std::string plat_label = file[0] == 'g' ? "Pickup_latitude" : "pickup_latitude";
+    
+
+    in.read_header(io::ignore_extra_column, pickup_label, plon_label, plat_label);
+    if (file[0] == 'g'){
+        std::string a = std::string(1,file[20]);
+        std::string b  = std::string(1,file[21]);
+        if (std::stoi(a+b) < 7){
+            in.col_order.push_back(-1);
+            in.col_order.push_back(-1);
+        }
+    }
+    std::cout << "File: " << file << std::endl;
+    
+    std::string node_name; float lon; float lat;
+    int bad_row = 0;
+
+    while(in.read_row(node_name, lon, lat)){
+        if ( lon == 0 || lat == 0)
+            bad_row++;
+        else{
+            box b(point(lon,lat), point(lon,lat));
+            rtree.insert(std::make_pair(b, node_name));
+        }
+    }
+    std::cout << bad_row << " incomplete rows" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ using box_type = geom::Box<point_type>;
 using index_type = index_::RTree<std::string, box_type, 16384>;
 
 #include "2015-data/fetch.hpp"
+#include "benchmark/bench.hpp"
+
+#include <boost/foreach.hpp>
 
 index_type
 create_index (const std::string& path) {
@@ -20,14 +23,52 @@ create_index (const std::string& path) {
     return tree;
 }
 
+
+bgi::rtree< value, bgi::quadratic<16> > 
+create_index_ (const std::string& path) {
+    auto a = fetch_locations_(path);
+    return a;
+}
+
 int main () {
-    index_type tree = create_index("../2015-data/j8/");
-    std::vector<std::string> result;
-    box_type box{{-73.922592163085938,40.754528045654297}, {-73.922592163085938,40.754528045654297}};
-    tree.query(box, std::back_inserter(result));
-    for (const auto& i : result) {
-        std::cout << i << '\n';
+
+    {
+        std::cout << "BOOST RTREE:\n";
+
+        auto t = create_index_("../2015-data/data/");
+        
+        // find values intersecting some area defined by a box
+        box query_box(point(-73.922592163085938, 40.754528045654297), point(-73.922592163085938, 40.754528045654297));
+        std::vector<value> result_s;
+        t.query(bgi::intersects(query_box), std::back_inserter(result_s));
+
+        std::cout << "spatial query result:" << std::endl;
+        for (auto a : result_s){
+            std::cout << 'x';
+        }
+
+        BOOST_FOREACH(value const& v, result_s)
+            std::cout << v.second << std::endl;
     }
+
+    std::cout << "_____________________________________________________" << std::endl;
+
+    {
+        std::cout << "IMPLEMENTED RTREE:\n";
+
+        index_type tree = create_index("../2015-data/data/");
+
+        std::vector<std::string> result;
+        box_type box{{-73.922592163085938,40.754528045654297}, {-73.922592163085938,40.754528045654297}};
+        tree.query(box, std::back_inserter(result));
+
+        for (const auto& i : result) {
+            std::cout << i << '\n';
+        }
+    
+    }
+
     std::cout << "excecution finished" << '\n';
+
     return 0;
 }
