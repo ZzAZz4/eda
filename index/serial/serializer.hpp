b705fb9c916dd514666ce226b88ee18a86c385cd
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-12-14 22:05:39 -0500

    suspicious, but it works... yay!

--- index/serial/serializer.hpp
+++ index/serial/serializer.hpp
@@ -13,60 +13,71 @@ struct serial::Serializer<index_::detail::RTreeBase<Record_, Box_, M_, m_>*> {
 
     inline static std::size_t written = 0;
 
-    template<class Archive>
-    static void serialize (const type& node, Archive& ar) {
-        Serializer<index_::detail::RTreeTag>::serialize(node->type, ar);
-        Serializer<typename base::size_type>::serialize(node->size, ar);
-        Serializer<typename base::box_type[base::capacity]>::serialize(node->boxes, ar);
-
+    template<class Archive, class DatFile>
+    static std::size_t serialize (const type& node, Archive& ar, DatFile& dat) {
         written++;
-        if (written % 100000 == 0) {
-            std::cout << "serializing... Written: " << written << std::endl;
-            ar.flush();
+        if (written % 10000 == 0) {
+            std::cout << "Serialized " << written << " nodes" << '\n';
         }
 
         switch (node->type) {
         case index_::detail::RTreeTag::LEAF: {
+            auto position = ar.tellp();
+            Serializer<index_::detail::RTreeTag>::serialize(node->type, ar);
+            Serializer<typename base::size_type>::serialize(node->size, ar);
+            Serializer<typename base::box_type[base::capacity]>::serialize(node->boxes, ar);
 
             auto cast = static_cast<leaf*>(node);
-            for (size_t i = 0; i < node->size; ++i) {
-                Serializer<typename leaf::record_type>::serialize(cast->records[i], ar);
+            for (std::size_t i = 0; i < node->size; ++i) {
+                std::size_t data_pos = dat.tellp();
+                Serializer<typename leaf::record_type>::serialize(cast->records[i], dat);
+                Serializer<std::size_t>::serialize(data_pos, ar);
             }
-            return;
+            return position;
         }
         case index_::detail::RTreeTag::INNER: {
             auto inner_node = static_cast<inner*>(node);
-            for (size_t i = 0; i < node->size; ++i) {
-                Serializer<type>::serialize(inner_node->records[i], ar);
+            std::size_t child_positions[base::capacity];
+            for (std::size_t i = 0; i < node->size; ++i) {
+                auto child_position = Serializer<type>::serialize(inner_node->records[i], ar, dat);
+                child_positions[i] = child_position;
             }
-            return;
+            auto position = ar.tellp();
+            Serializer<index_::detail::RTreeTag>::serialize(node->type, ar);
+            Serializer<typename base::size_type>::serialize(node->size, ar);
+            Serializer<typename base::box_type[base::capacity]>::serialize(node->boxes, ar);
+            Serializer<std::size_t[base::capacity]>::serialize(child_positions, ar);
+            return position;
         }
         default: throw std::out_of_range("Invalid type (wth?)");
         }
     }
 
-    template<class Archive>
-    static void recover (type& dump, Archive& ar) {
+    template<class Archive, class DatFile>
+    static void recover (type& dump, Archive& ar, DatFile& dat) {
         index_::detail::RTreeTag tag;
         Serializer<index_::detail::RTreeTag>::recover(tag, ar);
 
+        std::size_t children_pos[base::capacity];
         dump = tag == index_::detail::RTreeTag::LEAF ? (type) new leaf : (type) new inner;
         Serializer<typename base::size_type>::recover(dump->size, ar);
         Serializer<typename base::box_type[base::capacity]>::recover(dump->boxes, ar);
+        Serializer<std::size_t[base::capacity]>::recover(children_pos, ar);
 
         switch (dump->type) {
         case index_::detail::RTreeTag::LEAF: {
             auto cast = static_cast<leaf*>(dump);
-            for (size_t i = 0; i < dump->size; ++i) {
-                Serializer<typename leaf::record_type>::recover(cast->records[i], ar);
+            for (std::size_t i = 0; i < dump->size; ++i) {
+                dat.seekg(children_pos[i]);
+                Serializer<typename leaf::record_type>::recover(cast->records[i], dat);
             }
-
             return;
         }
         case index_::detail::RTreeTag::INNER: {
-            auto inner_node = static_cast<inner*>(dump);
-            for (size_t i = 0; i < dump->size; ++i) {
-                Serializer<type>::recover(inner_node->records[i], ar);
+            auto cast = static_cast<inner*>(dump);
+            for (std::size_t i = 0; i < dump->size; ++i) {
+                ar.seekg(children_pos[i]);
+                Serializer<typename inner::record_type>::recover(cast->records[i], ar, dat);
             }
             return;
         }
@@ -79,20 +90,26 @@ template<class Record_, class Box_, size_t M_, size_t m_>
 struct serial::Serializer<index_::RTree<Record_, Box_, M_, m_>> {
     using type = index_::RTree<Record_, Box_, M_, m_>;
 
-    template<class Archive>
-    static void serialize (const type& val, Archive& ar) {
+    template<class Archive, class DatFile>
+    static void serialize (const type& val, Archive& ar, DatFile& dat) {
         Serializer<char>::serialize(val.root == nullptr ? '\0' : '\1', ar);
         if (val.root) {
-            Serializer<typename type::base_node*>::serialize(val.root, ar);
+            Serializer<std::size_t>::serialize(0, ar);
+            std::size_t position = Serializer<typename type::base_node*>::serialize(val.root, ar, dat);
+            ar.seekp(1);
+            Serializer<std::size_t>::serialize(position, ar);
         }
     }
 
-    template<class Archive>
-    static void recover (type& dump, Archive& ar) {
+    template<class Archive, class DatFile>
+    static void recover (type& dump, Archive& ar, DatFile& dat) {
         char any_node;
+        std::size_t root_pos;
         Serializer<char>::recover(any_node, ar);
         if (any_node) {
-            Serializer<typename type::base_node*>::recover(dump.root, ar);
+            Serializer<std::size_t>::recover(root_pos, ar);
+            ar.seekg(root_pos);
+            Serializer<typename type::base_node*>::recover(dump.root, ar, dat);
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,45 +3,76 @@
 #include "index/rtree.hpp"
 #include "index/serial/serializer.hpp"
 #include "2015-data/fetch.hpp"
+#include "index/serial/serial_query.hpp"
 
 using point_type = geom::Point<float, 2>;
 using box_type = geom::Box<point_type>;
 using index_type = index_::RTree<std::string, box_type, 64>;
 
-void write() {
+void write () {
     std::cout << "Create\n";
     index_type index;
-    fetch_locations<index_type>("../2015-data/data/", index);
+    fetch_locations<index_type>("../2015-data/j8/", index);
     std::cout << "Created: " << index_type::created << "entries" << std::endl;
     std::cout << "Inserted: " << index_type::base_node::allocated << "entries" << std::endl;
 
     std::vector<std::string> res;
-    index.query(box_type({-73.958816528320312,40.716823577880859}, {-73.958816528320312,40.716823577880859}), std::back_inserter(res));
-    for (const auto &i : res) std::cout << i << '\n';
+    index.query(
+        box_type(
+            { -73.922592163085938, 40.754528045654297 },
+            { -73.922592163085938, 40.754528045654297 }),
+        std::back_inserter(res));
+    for (const auto& i : res) {
+        std::cout << i << '\n';
+    }
 
     std::cout << "Serialize\n";
-    std::ofstream os("../tree.bin", std::ios::binary);
-    serial::Serializer<index_type>::serialize(index, os);
+    std::ofstream ar("../tree.bin", std::ios::binary);
+    std::ofstream dat("../tree.dat", std::ios::binary);
+    serial::Serializer<index_type>::serialize(index, ar, dat);
     std::cout << "Destroy!\n";
 }
 
-void read() {
+void read () {
     std::cout << "Reload" << std::endl;
     index_type index;
-    std::ifstream is("../tree.bin", std::ios::binary);
-    serial::Serializer<index_type>::recover(index, is);
+    std::ifstream ar("../tree.bin", std::ios::binary);
+    std::ifstream dat("../tree.dat", std::ios::binary);
+    serial::Serializer<index_type>::recover(index, ar, dat);
     std::cout << "Reloaded" << std::endl;
 
     std::vector<std::string> res;
-    index.query(box_type({-73.958816528320312,40.716823577880859}, {-73.958816528320312,40.716823577880859}), std::back_inserter(res));
-    for (const auto &i : res) std::cout << i << '\n';
+    index.query(
+        box_type(
+            { -73.922592163085938, 40.754528045654297 },
+            { -73.922592163085938, 40.754528045654297 }),
+        std::back_inserter(res));
+    for (const auto& i : res) {
+        std::cout << i << '\n';
+    }
+}
+
+void just_query () {
+    std::cout << "Just query" << std::endl;
+    std::ifstream ar("../tree.bin", std::ios::binary);
+    std::ifstream dat("../tree.dat", std::ios::binary);
+    box_type box(
+        { -73.958816528320312, 40.716823577880859 }, { -73.958816528320312, 40.716823577880859 });
+
+    std::vector<std::string> res;
+    index_query<index_type>(ar, dat, box, std::back_inserter(res));
+
+    for (const auto& i : res) {
+        std::cout << i << '\n';
+    }
 }
 
 int main () {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    
-    write();
+
+    // write();
     std::cout << "Allocation check: " << index_type::base_node::allocated << std::endl;
-    read();
+//    read();
+    just_query();
 }
